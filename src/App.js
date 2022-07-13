a8ca90e91e93461be63e2c6f1f3d6b21b70517db
omerf18 <omerfussfeld@gmail.com>
2022-07-13 20:41:29 +0300

    header update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import AppHeader from './cmps/header/app-header';
 
 function App() {
--- src/cmps/header/app-header.jsx
+++ src/cmps/header/app-header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './app-header.scss';
+import logo from '../../assets/img/logo.png';
 
 export default function AppHeader() {
     return (
         <section className="main-header">
-            <div>LOGO</div>
+            <img className="logo" src={logo} alt="Logo" />
             <div className='navbar'>
                 <div>Signup</div>
                 <div>Map</div>
